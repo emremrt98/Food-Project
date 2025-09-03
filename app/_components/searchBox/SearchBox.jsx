e5ea86024ad5bde958513c4d6ba7ff509485ad5e
emremert <emre.mrt98@gmail.com>
2025-09-03 23:27:07 +0300

    Makro-07 Added search function

--- app/_components/searchBox/SearchBox.jsx
+++ app/_components/searchBox/SearchBox.jsx
@@ -4,127 +4,152 @@ import Container from '../container/Container'
 import { GiKnifeFork } from "react-icons/gi";
 import { CgClose } from "react-icons/cg";
 import useClickOutside from '@/app/_hooks/useClickOutside';
+import { useRouter } from 'next/navigation';
 
 export default function SearchBox() {
+    const router = useRouter()
+    const [selectedVal, setSelectedVal] = useState("");
     const [text, setText] = useState("");
     const [show, setShow] = useState(false);
+    const [skipEffect, setSkipEffect] = useState(false);
     const [datas, setDatas] = useState([]);
     const autocompleteDatas = [
-        { label: "Hamburger", value: "Hamburger" },
-        { label: "Lahmacun", value: "Lahmacun" },
-        { label: "Döner", value: "Döner" },
-        { label: "Pide", value: "Pide" },
-        { label: "Poğaça Tarifleri", value: "Poğaça Tarifleri" },
-        { label: "Salata Tarifleri", value: "Salata Tarifleri" },
-        { label: "Tavuk Salatası", value: "Tavuk Salatası" },
-        { label: "Akdeniz Salatası", value: "Akdeniz Salatası" },
-        { label: "Sezar Salatası", value: "Sezar Salatası" },
-        { label: "Çoban Salatası", value: "Çoban Salatası" },
-        { label: "Ton Balıklı Salata", value: "Ton Balıklı Salata" },
-        { label: "Kinoalı Salata", value: "Kinoalı Salata" },
-        { label: "Cheeseburger", value: "Cheeseburger" },
-        { label: "Mantar Burger", value: "Mantar Burger" },
-        { label: "Vejetaryen Burger", value: "Vejetaryen Burger" },
-        { label: "Falafel Burger", value: "Falafel Burger" },
-        { label: "Tavuk Şiş", value: "Tavuk Şiş" },
-        { label: "Tavuk Sote", value: "Tavuk Sote" },
-        { label: "Tavuk Fajita", value: "Tavuk Fajita" },
-        { label: "Tavuk Döner", value: "Tavuk Döner" },
-        { label: "Tavuk Tandır", value: "Tavuk Tandır" },
-        { label: "Tavuk Kanat", value: "Tavuk Kanat" },
-        { label: "Ballı Hardallı Tavuk", value: "Ballı Hardallı Tavuk" },
-        { label: "İnegöl Köfte", value: "İnegöl Köfte" },
-        { label: "Kaşarlı Köfte", value: "Kaşarlı Köfte" },
-        { label: "Izgara Köfte", value: "Izgara Köfte" },
-        { label: "Adana Kebap", value: "Adana Kebap" },
-        { label: "Urfa Kebap", value: "Urfa Kebap" },
-        { label: "İskender Kebap", value: "İskender Kebap" },
-        { label: "Beyti Kebap", value: "Beyti Kebap" },
-        { label: "Çöp Şiş", value: "Çöp Şiş" },
-        { label: "Pizza Margherita", value: "Pizza Margherita" },
-        { label: "Pizza Pepperoni", value: "Pizza Pepperoni" },
-        { label: "Dört Peynirli Pizza", value: "Dört Peynirli Pizza" },
-        { label: "Karışık Pizza", value: "Karışık Pizza" },
-        { label: "Makarna Napoliten", value: "Makarna Napoliten" },
-        { label: "Makarna Bolonez", value: "Makarna Bolonez" },
-        { label: "Fettucine Alfredo", value: "Fettucine Alfredo" },
-        { label: "Pesto Makarna", value: "Pesto Makarna" },
-        { label: "Mercimek Çorbası", value: "Mercimek Çorbası" },
-        { label: "Ezogelin Çorbası", value: "Ezogelin Çorbası" },
-        { label: "Tavuk Suyu Çorba", value: "Tavuk Suyu Çorba" },
-        { label: "Domates Çorbası", value: "Domates Çorbası" },
-        { label: "Menemen", value: "Menemen" },
-        { label: "Sucuklu Yumurta", value: "Sucuklu Yumurta" },
-        { label: "Karışık Omlet", value: "Karışık Omlet" },
-        { label: "Pankek", value: "Pankek" },
-        { label: "French Toast", value: "French Toast" },
-        { label: "Somon Izgara", value: "Somon Izgara" },
-        { label: "Levrek Izgara", value: "Levrek Izgara" },
-        { label: "Hamsi Tava", value: "Hamsi Tava" },
-        { label: "Kalamar Tava", value: "Kalamar Tava" },
-        { label: "Karides Güveç", value: "Karides Güveç" },
-        { label: "Kuru Fasulye", value: "Kuru Fasulye" },
-        { label: "Nohut Yemeği", value: "Nohut Yemeği" },
-        { label: "Barbunya Pilaki", value: "Barbunya Pilaki" },
-        { label: "Zeytinyağlı Enginar", value: "Zeytinyağlı Enginar" },
-        { label: "Yaprak Sarma", value: "Yaprak Sarma" },
-        { label: "Şehriyeli Pilav", value: "Şehriyeli Pilav" },
-        { label: "Bulgur Pilavı", value: "Bulgur Pilavı" },
-        { label: "Mantı", value: "Mantı" },
-        { label: "Patates Kızartması", value: "Patates Kızartması" },
-        { label: "Fırın Patates", value: "Fırın Patates" },
-        { label: "Patates Püresi", value: "Patates Püresi" },
-        { label: "Patates Salatası", value: "Patates Salatası" },
-        { label: "Et Sote", value: "Et Sote" },
-        { label: "Dana Antrikot", value: "Dana Antrikot" },
-        { label: "Bonfile Mantar Soslu", value: "Bonfile Mantar Soslu" },
-        { label: "Et Döner", value: "Et Döner" },
-        { label: "Kavurma", value: "Kavurma" },
-        { label: "Hünkar Beğendi", value: "Hünkar Beğendi" },
-        { label: "Tas Kebabı", value: "Tas Kebabı" },
-        { label: "Musakka", value: "Musakka" },
-        { label: "İmam Bayıldı", value: "İmam Bayıldı" },
-        { label: "Karnıyarık", value: "Karnıyarık" },
-        { label: "Kabak Mücver", value: "Kabak Mücver" },
-        { label: "Taze Fasulye", value: "Taze Fasulye" },
-        { label: "Çiğköfte", value: "Çiğköfte" },
-        { label: "İçli Köfte", value: "İçli Köfte" },
-        { label: "Mantarlı Risotto", value: "Mantarlı Risotto" },
-        { label: "Sushi California Roll", value: "Sushi California Roll" },
-        { label: "Pad Thai", value: "Pad Thai" },
-        { label: "Burrito", value: "Burrito" },
-        { label: "Taco", value: "Taco" },
-        { label: "Quesadilla", value: "Quesadilla" },
-        { label: "Guacamole", value: "Guacamole" },
-        { label: "Humus", value: "Humus" },
-        { label: "Baba Ganuş", value: "Baba Ganuş" },
-        { label: "Tabbouleh", value: "Tabbouleh" },
-        { label: "Shakshuka", value: "Shakshuka" },
-        { label: "Baklava", value: "Baklava" },
-        { label: "Künefe", value: "Künefe" },
-        { label: "Sütlaç", value: "Sütlaç" },
-        { label: "Kazandibi", value: "Kazandibi" },
-        { label: "Profiterol", value: "Profiterol" },
-        { label: "Tiramisu", value: "Tiramisu" },
-        { label: "Dondurma Çikolatalı", value: "Dondurma Çikolatalı" },
-        { label: "Brownie", value: "Brownie" },
-        { label: "Cheesecake", value: "Cheesecake" }
+        { label: "Hamburger", value: "hamburger" },
+        { label: "Lahmacun", value: "lahmacun" },
+        { label: "Döner", value: "doner" },
+        { label: "Pide", value: "pide" },
+        { label: "Poğaça Tarifleri", value: "pogaca-tarifleri" },
+        { label: "Salata Tarifleri", value: "salata-tarifleri" },
+        { label: "Tavuk Salatası", value: "tavuk-salatasi" },
+        { label: "Akdeniz Salatası", value: "akdeniz-salatasi" },
+        { label: "Sezar Salatası", value: "sezar-salatasi" },
+        { label: "Çoban Salatası", value: "coban-salatasi" },
+        { label: "Ton Balıklı Salata", value: "ton-balikli-salata" },
+        { label: "Kinoalı Salata", value: "kinoali-salata" },
+        { label: "Cheeseburger", value: "cheeseburger" },
+        { label: "Mantar Burger", value: "mantar-burger" },
+        { label: "Vejetaryen Burger", value: "vejetaryen-burger" },
+        { label: "Falafel Burger", value: "falafel-burger" },
+        { label: "Tavuk Şiş", value: "tavuk-sis" },
+        { label: "Tavuk Sote", value: "tavuk-sote" },
+        { label: "Tavuk Fajita", value: "tavuk-fajita" },
+        { label: "Tavuk Döner", value: "tavuk-doner" },
+        { label: "Tavuk Tandır", value: "tavuk-tandir" },
+        { label: "Tavuk Kanat", value: "tavuk-kanat" },
+        { label: "Ballı Hardallı Tavuk", value: "balli-hardalli-tavuk" },
+        { label: "İnegöl Köfte", value: "inegol-kofte" },
+        { label: "Kaşarlı Köfte", value: "kasarli-kofte" },
+        { label: "Izgara Köfte", value: "izgara-kofte" },
+        { label: "Adana Kebap", value: "adana-kebap" },
+        { label: "Urfa Kebap", value: "urfa-kebap" },
+        { label: "İskender Kebap", value: "iskender-kebap" },
+        { label: "Beyti Kebap", value: "beyti-kebap" },
+        { label: "Çöp Şiş", value: "cop-sis" },
+        { label: "Pizza Margherita", value: "pizza-margherita" },
+        { label: "Pizza Pepperoni", value: "pizza-pepperoni" },
+        { label: "Dört Peynirli Pizza", value: "dort-peynirli-pizza" },
+        { label: "Karışık Pizza", value: "karisik-pizza" },
+        { label: "Makarna Napoliten", value: "makarna-napoliten" },
+        { label: "Makarna Bolonez", value: "makarna-bolonez" },
+        { label: "Fettucine Alfredo", value: "fettucine-alfredo" },
+        { label: "Pesto Makarna", value: "pesto-makarna" },
+        { label: "Mercimek Çorbası", value: "mercimek-corbasi" },
+        { label: "Ezogelin Çorbası", value: "ezogelin-corbasi" },
+        { label: "Tavuk Suyu Çorba", value: "tavuk-suyu-corba" },
+        { label: "Domates Çorbası", value: "domates-corbasi" },
+        { label: "Menemen", value: "menemen" },
+        { label: "Sucuklu Yumurta", value: "sucuklu-yumurta" },
+        { label: "Karışık Omlet", value: "karisik-omlet" },
+        { label: "Pankek", value: "pankek" },
+        { label: "French Toast", value: "french-toast" },
+        { label: "Somon Izgara", value: "somon-izgara" },
+        { label: "Levrek Izgara", value: "levrek-izgara" },
+        { label: "Hamsi Tava", value: "hamsi-tava" },
+        { label: "Kalamar Tava", value: "kalamar-tava" },
+        { label: "Karides Güveç", value: "karides-guvec" },
+        { label: "Kuru Fasulye", value: "kuru-fasulye" },
+        { label: "Nohut Yemeği", value: "nohut-yemegi" },
+        { label: "Barbunya Pilaki", value: "barbunya-pilaki" },
+        { label: "Zeytinyağlı Enginar", value: "zeytinyagli-enginar" },
+        { label: "Yaprak Sarma", value: "yaprak-sarma" },
+        { label: "Şehriyeli Pilav", value: "sehriyeli-pilav" },
+        { label: "Bulgur Pilavı", value: "bulgur-pilavi" },
+        { label: "Mantı", value: "manti" },
+        { label: "Patates Kızartması", value: "patates-kizartmasi" },
+        { label: "Fırın Patates", value: "firin-patates" },
+        { label: "Patates Püresi", value: "patates-puresi" },
+        { label: "Patates Salatası", value: "patates-salatasi" },
+        { label: "Et Sote", value: "et-sote" },
+        { label: "Dana Antrikot", value: "dana-antrikot" },
+        { label: "Bonfile Mantar Soslu", value: "bonfile-mantar-soslu" },
+        { label: "Et Döner", value: "et-doner" },
+        { label: "Kavurma", value: "kavurma" },
+        { label: "Hünkar Beğendi", value: "hunkar-begendi" },
+        { label: "Tas Kebabı", value: "tas-kebabi" },
+        { label: "Musakka", value: "musakka" },
+        { label: "İmam Bayıldı", value: "imam-bayildi" },
+        { label: "Karnıyarık", value: "karniyarik" },
+        { label: "Kabak Mücver", value: "kabak-mucver" },
+        { label: "Taze Fasulye", value: "taze-fasulye" },
+        { label: "Çiğköfte", value: "cigkofte" },
+        { label: "İçli Köfte", value: "icli-kofte" },
+        { label: "Mantarlı Risotto", value: "mantarli-risotto" },
+        { label: "Sushi California Roll", value: "sushi-california-roll" },
+        { label: "Pad Thai", value: "pad-thai" },
+        { label: "Burrito", value: "burrito" },
+        { label: "Taco", value: "taco" },
+        { label: "Quesadilla", value: "quesadilla" },
+        { label: "Guacamole", value: "guacamole" },
+        { label: "Humus", value: "humus" },
+        { label: "Baba Ganuş", value: "baba-ganus" },
+        { label: "Tabbouleh", value: "tabbouleh" },
+        { label: "Shakshuka", value: "shakshuka" },
+        { label: "Baklava", value: "baklava" },
+        { label: "Künefe", value: "kunefe" },
+        { label: "Sütlaç", value: "sutlac" },
+        { label: "Kazandibi", value: "kazandibi" },
+        { label: "Profiterol", value: "profiterol" },
+        { label: "Tiramisu", value: "tiramisu" },
+        { label: "Dondurma Çikolatalı", value: "dondurma-cikolata" },
+        { label: "Brownie", value: "brownie" },
+        { label: "Cheesecake", value: "cheesecake" }
     ];
+
     const autocompleteRef = useClickOutside(() => setShow(false));
 
     useEffect(() => {
+
+        if (skipEffect) {
+            setSkipEffect(false);
+            return;
+        }
         if (text.length >= 3) {
-            const result = autocompleteDatas.filter((autocompleteData) => autocompleteData.value.toLowerCase().includes(text.toLowerCase()))
+            const result = autocompleteDatas.filter((autocompleteData) => autocompleteData.label.toLowerCase().includes(text.toLowerCase()))
             setDatas(result);
             setShow(true);
         } else {
             setDatas([]);
             setShow(false);
         }
+
     }, [text])
 
     const setAutocompleteData = (event) => {
         setText(event.target.value);
+    }
+
+    const selectInputData = (event) => {
+        const { label, value } = event.target.dataset
+        setSelectedVal(value);
+        setText(label)
+        setDatas([]);
+        setShow(false);
+        setSkipEffect(true);
+    }
+
+    const handleSearch = () => {
+        console.log("Value : ",);
+        router.push(`/search/?query=${selectedVal}`)
     }
 
     return (
@@ -142,19 +167,19 @@ export default function SearchBox() {
                             </div>
                             <div className='flex flex-col gap-1 flex-1'>
                                 <span className='text-sm text-secondary font-normal'>Ne yemek istiyorsun?</span>
-                                <input onChange={setAutocompleteData} className='outline-none text-base text-primary font-medium placeholder:text-primary placeholder:text-base placeholder:font-medium' name='autocomplete' placeholder='Hamburger, pizza, kebap veya lahmacun arayın' value={text} type="text" />
+                                <input autoComplete='off' onChange={setAutocompleteData} className='outline-none text-base text-primary font-medium placeholder:text-primary placeholder:text-base placeholder:font-medium' name='autocomplete' placeholder='Hamburger, pizza, kebap veya lahmacun arayın' value={text} type="text" />
                             </div>
                             {
                                 (datas.length && show) > 0 &&
                                 <ul className='bg-white shadow-xl rounded-[8px] w-full absolute min-h-[50px] max-h-[350px] overflow-y-auto top-[65px] left-12 border border-weak-border z-10'>
                                     {
-                                        datas.map((data, index) => <li className='hover:bg-box cursor-pointer p-4 rounded-[8px]' key={index}>{data.label}</li>)
+                                        datas.map((data, index) => <li data-value={data.value} data-label={data.label} onClick={selectInputData} className='hover:bg-box cursor-pointer p-4 rounded-[8px]' key={index}>{data.label}</li>)
                                     }
                                 </ul>
                             }
                         </div>
                         <div>
-                            <button className='bg-main text-white text-base font-medium py-3 px-4 rounded-[8px] cursor-pointer'>Yemekleri Listele</button>
+                            <button onClick={handleSearch} className='bg-main text-white text-base font-medium py-3 px-4 rounded-[8px] cursor-pointer'>Yemekleri Listele</button>
                         </div>
                     </div>
                     <div>

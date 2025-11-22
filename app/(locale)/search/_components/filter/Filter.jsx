e01530b0c6bc1f85ec765dab53054b6eab02e8b1
Emre Mert <emre.mrt98@gmail.com>
2025-11-22 18:49:23 +0300

    Makro-11 set filter

--- app/(locale)/search/_components/filter/Filter.jsx
+++ app/(locale)/search/_components/filter/Filter.jsx
@@ -1,8 +1,57 @@
-import React from 'react'
+'use client'
+
+import React, { useReducer } from 'react'
 import { HiOutlineChevronDown } from "react-icons/hi";
 import { IoCheckmarkOutline } from "react-icons/io5";
 import { FaCheck } from "react-icons/fa";
+
+const filterReducer = (state, action) => {
+    switch (action.type) {
+        case "Set_Filter":
+            return {
+                ...state,
+                [action.payload.name]: action.payload.val,
+            }
+
+        case "Clear_Filter":
+            return {
+                recipes: null,
+                foodPoint: null,
+                servingSizes: null
+            }
+    }
+}
+
 export default function Filter() {
+
+    const [state, dispatch] = useReducer(filterReducer, {
+        recipes: null,
+        foodPoint: null,
+        servingSizes: null
+    });
+
+    const handleChangeFilter = (event) => {
+        const name = event.target.name;
+        const val = event.target.dataset.val;
+        dispatch({
+            type: "Set_Filter",
+            payload: {
+                name,
+                val
+            }
+        })
+    }
+
+    const applyFilter = () => {
+        console.log("State değeri : ", state)
+    }
+
+    const clearFilter = () => {
+        dispatch({
+            type: "Clear_Filter",
+        })
+        console.log("State değerleri temizlendi : ", state)
+    }
     return (
         <div className='filter'>
             <div className='flex justify-between items-center'>
@@ -17,11 +66,11 @@ export default function Filter() {
                     </div>
                     <div className='flex flex-col gap-2 mt-4'>
                         <label className='filter__radio-btn'>
-                            <input type="radio" hidden name='custom-radio' />
+                            <input onChange={handleChangeFilter} type="radio" hidden data-val="popular-recipes" name='recipes' />
                             <span>Popüler tarifler</span>
                         </label>
                         <label className='filter__radio-btn'>
-                            <input type="radio" hidden name='custom-radio' />
+                            <input onChange={handleChangeFilter} type="radio" hidden data-val="new-recipes" name='recipes' />
                             <span>Yeni tarifler</span>
                         </label>
                     </div>
@@ -35,25 +84,25 @@ export default function Filter() {
                     </div>
                     <div className='flex flex-col gap-2 mt-4'>
                         <label className='filter__radio-btn'>
-                            <input type="radio" hidden name='point-radio-btn' />
+                            <input onChange={handleChangeFilter} type="radio" hidden data-val="9+" name='foodPoint' />
                             <span>
                                 9 ve üzeri
                             </span>
                         </label>
                         <label className='filter__radio-btn'>
-                            <input type="radio" hidden name='point-radio-btn' />
+                            <input onChange={handleChangeFilter} type="radio" hidden data-val="8+" name='foodPoint' />
                             <span>
                                 8 ve üzeri
                             </span>
                         </label>
                         <label className='filter__radio-btn'>
-                            <input type="radio" hidden name='point-radio-btn' />
+                            <input onChange={handleChangeFilter} type="radio" hidden data-val="7+" name='foodPoint' />
                             <span>
                                 7 ve üzeri
                             </span>
                         </label>
                         <label className='filter__radio-btn'>
-                            <input type="radio" hidden name='point-radio-btn' />
+                            <input onChange={handleChangeFilter} type="radio" hidden data-val="6+" name='foodPoint' />
                             <span>
                                 6 ve üzeri
                             </span>
@@ -69,28 +118,28 @@ export default function Filter() {
                     </div>
                     <div className='flex flex-col gap-2 mt-4'>
                         <label className='filter__checkbox-btn'>
-                            <input type="checkbox" hidden />
+                            <input onChange={handleChangeFilter} data-val="1-2" name='servingSizes' type="checkbox" hidden />
                             <span>
                                 1-2 kişilik
                                 <FaCheck size={13} className='filter__checkbox-btn__icon' />
                             </span>
                         </label>
                         <label className='filter__checkbox-btn'>
-                            <input type="checkbox" hidden />
+                            <input onChange={handleChangeFilter} data-val="2-4" name='servingSizes' type="checkbox" hidden />
                             <span>
                                 2-4 kişilik
                                 <FaCheck size={13} className='filter__checkbox-btn__icon' />
                             </span>
                         </label>
                         <label className='filter__checkbox-btn'>
-                            <input type="checkbox" hidden />
+                            <input onChange={handleChangeFilter} data-val="4-6" name='servingSizes' type="checkbox" hidden />
                             <span>
                                 4-6 kişilik
                                 <FaCheck size={13} className='filter__checkbox-btn__icon' />
                             </span>
                         </label>
                         <label className='filter__checkbox-btn'>
-                            <input type="checkbox" hidden />
+                            <input onChange={handleChangeFilter} data-val="6-8" name='servingSizes' type="checkbox" hidden />
                             <span>
                                 6-8 kişilik
                                 <FaCheck size={13} className='filter__checkbox-btn__icon' />
@@ -101,10 +150,10 @@ export default function Filter() {
             </div>
             <div className='flex gap-4'>
                 <div className='flex-1'>
-                    <button className='w-full border border-main text-main text-base font-medium px-4 py-3 rounded-[8px]'>Temizle</button>
+                    <button onClick={clearFilter} className='cursor-pointer w-full border border-main text-main text-base font-medium px-4 py-3 rounded-[8px]'>Temizle</button>
                 </div>
                 <div className='flex-1'>
-                    <button className='w-full border border-main text-white bg-main text-base font-medium px-4 py-3 rounded-[8px]'>Uygula</button>
+                    <button onClick={applyFilter} className='cursor-pointer w-full border border-main text-white bg-main text-base font-medium px-4 py-3 rounded-[8px]'>Uygula</button>
                 </div>
             </div>
         </div>
